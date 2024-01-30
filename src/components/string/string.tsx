f38acbdbd30718461e57f8f345728e9a636c2dfc
Kseniya <xenianevarenyh@yandex.ru>
2024-01-30 10:50:42 +0500

    split logic and component of string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -4,13 +4,15 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
-import { ElementStates } from "../../types/element-states";
+
 import { DELAY_IN_MS } from "../../constants/delays";
+import { ReverseString, BackStringColour } from "../utils/utils";
 
 export const StringComponent: React.FC = () => {
   const [string, setString] = useState<string[]>([]);
   const [indexes, setIndexes] = useState<Array<number>>([]);
-  const [modified, setModified] = useState<string>();
+  const [modified, setModified] = useState<string>("noneModified");
+
   const changeString = (e: ChangeEvent<HTMLInputElement>) => {
     setModified("noneModified");
     setIndexes([]);
@@ -22,34 +24,23 @@ export const StringComponent: React.FC = () => {
     setString(arr);
     if (start === Math.floor(string.length / 2)) {
       setIndexes([]);
-
       return;
     }
     setIndexes([start, end]);
   };
 
-  const color = (i: number): ElementStates => {
-    if (indexes.indexOf(i) >= 0 && modified !== "modified") {
-      return ElementStates.Changing;
-    } else if (i < indexes[0] || i > indexes[1] || modified === "modified") {
-      return ElementStates.Modified;
-    } else {
-      return ElementStates.Default;
-    }
-  };
-
   const reverseString = () => {
     setModified("loading");
+    const allString = ReverseString(string);
+
     let start = 0;
     let end = string.length - 1;
-    let arr = [...string];
-    while (start < end) {
-      changePosition(arr, start, end);
-      let sorted = [...arr];
+
+    while (allString[start]) {
       let startCopy = start;
       let startEnd = end;
       setTimeout(
-        () => addNew(sorted, startCopy, startEnd),
+        () => addNew(allString[start - 1], startCopy, startEnd),
         DELAY_IN_MS + DELAY_IN_MS * start
       );
       start++;
@@ -61,17 +52,7 @@ export const StringComponent: React.FC = () => {
         Math.floor(string.length / 2),
       ]);
       setModified("modified");
-    }, (DELAY_IN_MS + start) * DELAY_IN_MS);
-  };
-
-  const changePosition = (
-    arr: string[],
-    firstIndex: number,
-    secondIndex: number
-  ): void => {
-    const temp = arr[firstIndex];
-    arr[firstIndex] = arr[secondIndex];
-    arr[secondIndex] = temp;
+    }, (1 + start) * DELAY_IN_MS);
   };
 
   return (
@@ -92,7 +73,11 @@ export const StringComponent: React.FC = () => {
       </div>
       <div className={styles.circles}>
         {string.map((item, index) => (
-          <Circle letter={item} state={color(index)} />
+          <Circle
+            letter={item}
+            state={BackStringColour(index, indexes, modified)}
+            key={index}
+          />
         ))}
       </div>
     </SolutionLayout>
