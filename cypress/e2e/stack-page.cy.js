e39bd677498656e57ecd65a0808d3cb3f751423f
Kseniya <xenianevarenyh@yandex.ru>
2024-02-29 05:53:03 +0500

    add stack test

--- cypress/e2e/stack-page.cy.js
+++ cypress/e2e/stack-page.cy.js
@@ -1,12 +1,58 @@
 describe("should cheeck disabled buttons", () => {
   beforeEach(function () {
     cy.visit("http://localhost:3000/stack");
+    cy.get("input").clear("");
   });
 
-  it("passes", () => {
+  it("should cheeck disabled buttons", () => {
     cy.get("input").clear("");
     cy.get(".add_button").should("be.disabled");
     cy.get(".delete_button").should("be.disabled");
     cy.get(".remove_button").should("be.disabled");
+  });
+
+  it("should correctly delete element from stack", () => {
+    cy.get("input").type("1");
+    cy.get(".add_button").click();
+
+    cy.get("input").type("2");
+    cy.get(".add_button").click();
+
+    cy.get(".delete_button").click();
+
+    cy.get("div[class*=circle_circle]").should("contain", "1");
+    cy.get("div[class*=circle_circle]").not("contain", "12");
+  });
+
+  it("should correctly delete element from stack", () => {
+    cy.get("input").type("1");
+    cy.get(".add_button").click();
+
+    cy.get("div[class*=circle_changing]", { timeout: 0 });
+    cy.get("div[class*=circle_changing]").not();
+
+    cy.get("[class*=circle_content]").first().contains("top");
+    cy.get("[class*=circle_content]").last().contains("tail");
+
+    cy.get("input").type("1");
+    cy.get(".add_button").click();
+
+    cy.get("div[class*=circle_changing]", { timeout: 0 });
+    cy.get("div[class*=circle_changing]").not();
+
+    cy.get("[class*=circle_content]").first().contains("top");
+    cy.get("[class*=circle_content]").last().contains("tail");
+  });
+
+  it("stack should be empty after click button remove ", () => {
+    cy.get("input").type("1");
+    cy.get(".add_button").click();
+
+    cy.get("input").type("2");
+    cy.get(".add_button").click();
+
+    cy.get(".remove_button").click();
+
+    cy.get("div[class*=circle_circle]").should("not.exist");
   });
 });
--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -6,6 +6,7 @@ import { Circle } from "../ui/circle/circle";
 import { Input } from "../ui/input/input";
 import { ElementStates } from "../../types/element-states";
 import { IStack } from "../../types/stack";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 class Stack<T> implements IStack<T> {
   private container: T[] = [];
@@ -32,6 +33,7 @@ class Stack<T> implements IStack<T> {
 export const StackPage: React.FC = () => {
   const [queue, setQueue] = useState<IStack<string>>(new Stack());
   const [tail, setTail] = useState<number>(-1);
+  const [changing, setChanging] = useState<number>(-1);
   const [amount, setAmount] = useState<number>(0);
   const [value, setValue] = useState<string>("");
 
@@ -40,12 +42,20 @@ export const StackPage: React.FC = () => {
     console.log(queue.getStack());
     setAmount(amount + 1);
     setTail(tail + 1);
+    setChanging(tail + 1);
+    setTimeout(() => {
+      setChanging(-1);
+    }, SHORT_DELAY_IN_MS);
   };
 
   const deleteItem = () => {
     queue.pop();
     setAmount(amount - 1);
     setTail(tail - 1);
+    setChanging(tail - 1);
+    setTimeout(() => {
+      setChanging(-1);
+    }, SHORT_DELAY_IN_MS);
   };
 
   const removeItems = () => {
@@ -93,9 +103,10 @@ export const StackPage: React.FC = () => {
             letter={element ? element : ""}
             index={index}
             key={index}
-            head={tail === index ? "top" : ""}
+            head={index === 0 ? "top" : ""}
+            tail={tail === index ? "tail" : ""}
             state={
-              tail === index && amount
+              changing === index && amount
                 ? ElementStates.Changing
                 : ElementStates.Default
             }
